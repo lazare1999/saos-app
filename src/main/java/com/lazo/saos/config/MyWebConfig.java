b87e9419088359fefb7108590177cea129c89b57
lazo <lazarekvirtia@gmail.com>
2023-12-12 12:29:27 +0400

    Spring Security

---
+++ src/main/java/com/lazo/saos/config/MyWebConfig.java
@@ -0,0 +1,19 @@
+package com.lazo.saos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Lazo on 2023-12-11
+ */
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui/index.html");
+    }
+
+}
